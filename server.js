241ce1e76a540f95387fc4df41c70177aad82fe5
soraya-skavinski-thaddeus <sorayaskavinski@thaddeus.org>
2025-03-29 12:02:06 -0300

    some changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const MongoStore = require('connect-mongo');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database'); 
 const cors = require('cors');
